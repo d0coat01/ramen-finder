726d840ca4b504cfa80cdb69cee37fd9853c9dd8
Daniel Coats <d0coat01@louisville.edu>
2018-07-29 16:35:41 -0700

    Ramen Finder now includes ratings filter and displays address and Yelp image on Google maps.
    Also in this commit:
    - Marker changed from svg to default google marker.
    - Sidebar now includes toggle switch.
    - Error handling for search-ramen api call.
    - General code cleanup

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,13 +1,13 @@
 var markers = [];
-var map, polygon, infowindow;
-const COLORS = ["red", "blue", "green", "yellow"];
+var map, infowindow;
+
+/**
+* @description Creates a google Map object and Infowindow object
+*/
 function initMap() {
     const locations = [
-        { lat: -34.433253313346334, lng: 150.8911923828125 },
-        { lat: -34.885090952923406, lng: 150.6055478515625 },
-        { lat: -34.397, lng: 150.644 }
     ];
-    const location = { lat: 37.761549, lng: -122.440021 };
+    const location = { lat: 37.761549, lng: -122.440021 }; //center of San Francisco
     map = new google.maps.Map(document.getElementById('map'), {
         center: location,
         zoom: 12,
@@ -16,66 +16,70 @@ function initMap() {
     //addMarkers(locations);
     //const drawingmanager = addDrawingTools(map);
 }
-function addDrawingTools() {
-    var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_LEFT,
-            drawingModes: ['polygon']
-        },
-    });
-    drawingManager.setMap(map);
-    drawingManager.addListener("overlaycomplete", (event) => {
-        if (polygon) {
-            polygon.setMap(null);
-            hideListings();
-        }
-        drawingManager.setDrawingMode(null);
-        polygon = event.overlay;
-        polygon.setEditable(true);
-        areaWithinPolygon();
-        polygon.getPath().addListener("set_at", areaWithinPolygon);
-        polygon.getPath().addListener("insert_at", areaWithinPolygon);
-    });
-    document.getElementById("drawing-tools").addEventListener("click",
-        () => toggleDrawingTools(drawingManager));
-    return drawingManager;
-}
-function areaWithinPolygon() {
-    console.log(polygon);
-    const area = google.maps.geometry.spherical.computeArea(polygon.getPath());
-    infowindow.setContent(Number.parseFloat(area).toFixed(2) + " square meters");
-    var bounds = new google.maps.LatLngBounds();
-    polygon.getPath().forEach(function (element, index) { bounds.extend(element) });
-    infowindow.setPosition(bounds.getCenter().toJSON());
-    infowindow.open(map);
 
+/**
+* @description Toggles a ramen shop marker. If placing a marker,
+* parses the shop's yelp reviews, and attaches appropriate event listeners.
+* @constructor
+* @param {object} location - A ramen shop location object.
+*/
+function toggleMarker(location) {
+    //check if location exists in markers....if it does, remove it and return.
+    for (let i=0; i<markers.length; i++) {
+        if (markers[i].yelp.id === location.id) {
+            markers[i].setMap(null); //remove marker
+            markers.splice(i, 1);
+            return;
+        }
+    }
+
+    //grab desired Yelp info
+    const yelp = {
+        address: location.location.display_address,
+        image: location.image_url,
+        price: location.price,
+        name: location.name,
+        id: location.id
+    };
+
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        draggable: true,
+        title: 'First!',
+    });
+    marker.yelp = yelp;
+    marker.addListener('click', () => openWindow(marker, infowindow));
+    marker.addListener('click', () => setBounce(marker));
+    markers.push(marker);
 }
-function addMarkers(locations) {
-    for (let location of locations) {
-        const icon = 'http://maps.google.com/mapfiles/ms/icons/' + COLORS[Math.floor((Math.random() * COLORS.length))] + '-dot.png';
-        const marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true,
-            icon: icon,
-            animation: google.maps.Animation.DROP,
-            title: 'First!',
-        });
-        marker.addListener('click', () => openWindow(marker, infowindow));
-        markers.push(marker);
+
+/**
+* @description Sets the bounce animation for a marker
+* @constructor
+* @param {google.maps.Marker} marker - A Google Marker object
+*/
+function setBounce(marker) {
+    const animation = marker.getAnimation();
+    if (typeof animation === "undefined" || animation === null) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(() => { marker.setAnimation(null) }, 2000);
     }
 }
+
+/**
+* @description Displays an Infowindow over the selected marker.
+* @param {google.maps.Marker} marker -  A Google Marker object
+* @param {google.maps.InfoWindow} infowindow - A Google InfoWindow object
+*/
 function openWindow(marker, infowindow) {
-    infowindow.setContent(marker.getPosition().toString());
+    //convert marker yelp info into html
+    const address = "<div>" + marker.yelp.address.join("</div><div>") + "</div>";
+    const content = `<div class='marker'>
+    <img class='marker__image' src='${marker.yelp.image}'>
+        <div><strong>${marker.yelp.name}</strong>
+        ${address}
+    </div>`;
+    infowindow.setContent(content);
     infowindow.open(map, marker);
-}
-function toggleDrawingTools(drawingManager) {
-    if (drawingManager.map) {
-        drawingManager.setMap(null);
-    }
-    else {
-        drawingManager.setMap(map);
-    }
 }
---
+++ js/main.js
@@ -0,0 +1,91 @@
+/**
+* @description Ramen Finder's Knockout View Model
+*/
+function ramenViewModel() {
+  const self = this;
+
+  //knockout variables and constant definitions.
+  self.shops = ko.observableArray([]);
+  self.selectedShops = ko.observableArray([]);
+  self.ratings = ['All', 5, 4.5, 4, 3, 2, 1];
+  self.selectedRating = ko.observable('All');
+  self.showMenu = ko.observable(true);
+  self.errorMessage = ko.observable(false);
+
+  //get all ramen restaurants in San Francisco.
+  $.ajax({
+    url: 'http://localhost:3000/search-ramen',
+    method: 'GET',
+    dataType: 'json',
+  }).done((result) => {
+    self.errorMessage(false);
+    for (let shop of result.businesses) {
+      // Map rating to yelp rating image source.
+      const rating = shop.rating.toString();
+      const half_star = rating.search('.5') !== -1 ? '_half' : '';
+      const full_stars = rating[0];
+      shop.rating_img_src = 'images/yelp/small/small_' + full_stars + half_star + '.png';
+      shops.push(shop);
+      //default all shops to selected.
+      self.toggleShop(shop);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+    self.errorMessage('Uh oh! There was a problem cooking up the ramen shop list!')
+
+  });
+/**
+* @description Toggles the showMenu boolean. Used for showing/hiding ramen list sidebar.
+*/
+  self.toggleMenu = function() {
+    self.showMenu(!self.showMenu());
+  }
+
+/**
+* @description Whenever the rating filter is changed, display the appropriate ramen shops.
+*/
+  self.ratingChange = function() {
+    const rating = self.selectedRating() === 'All' ? 0 : parseFloat(self.selectedRating());
+    for(let shop of self.shops()) {
+      const selected = self.selectedShops().indexOf(shop.id) !== -1;
+      if(!selected && shop.rating >= rating) {
+        //shop is not selected but should be.
+        self.toggleShop(shop);
+      }
+      if(selected && shop.rating < rating) {
+        //shop is selected but should not be.
+        self.toggleShop(shop);
+      }
+    }
+  };
+/**
+* @description Toggles a ramen shop between selected and not selected.
+* @constructor
+* @param {object} shop - A ramen shop object.
+*/
+  self.toggleShop = function (shop) {
+    const selected = self.selectedShops().indexOf(shop.id) === -1;
+    if (selected) {
+      self.selectedShops.push(shop.id);
+    }
+    else {
+      self.selectedShops.remove(shop.id);
+    }
+    displayShop(shop);
+  };
+}
+//End of Ramen View Model
+
+/**
+* @description A hook into google map markers. Toggles the marker of the target ramen shop.
+* @constructor
+* @param {object} shop - A ramen shop object.
+*/
+function displayShop(shop) {
+  shop.lat = parseFloat(shop.coordinates.latitude);
+  shop.lng = parseFloat(shop.coordinates.longitude);
+  toggleMarker(shop);
+}
+
+ko.applyBindings(ramenViewModel);
